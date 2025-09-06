f00202ac307b9c818a2e273ae2f6695129c08bbc
Anik <anikdas210605@gmail.com>
2025-09-06 21:45:03 +0530

    added the frontend link to avoid cors issue

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5100;
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:3001'],
+  origin: ['http://localhost:3000', 'http://localhost:3001','https://hrcc-recruitments.vercel.app/','http://hrcc-recruitments.vercel.app'],
   credentials: true
 }));
 app.use(express.json());
